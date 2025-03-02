3a36ec384b193c1179a607650d919f5230872a36
evanvidel <evanvidel@gmail.com>
2025-03-01 22:16:02 -0300

    exercises, navigation test

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Teste() {
+  return <div>
+
+    <h1>Pagina de Produtos</h1>
+
+    <Link href="/produtos/teste" >
+    Navegar para Teste
+    </Link>
+  </div>;
+}
